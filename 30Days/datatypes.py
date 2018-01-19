7bb8968749762535dd7ebf1f3de7937502980704
OpticGenius <opticgenius@hotmail.com>
2018-01-20 00:43:07 +1100

    Changed directory around

---
+++ 30Days/datatypes.py
@@ -0,0 +1,26 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+num_int = int(input())
+num_double = float(input())
+string = input()
+
+# Declare second integer, double, and String variables.
+
+
+# Read and save an integer, double, and String to your variables.
+num_int = int(input())
+num_double = float(input())
+string = input()
+
+
+# Print the sum of both integer variables on a new line.
+print(i + num_int)
+
+# Print the sum of the double variables on a new line.
+print(d + num_double)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + string)
