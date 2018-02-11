f48b9a2cd10dc54320ec870eb70f0837735fe249
OpticGenius <opticgenius@hotmail.com>
2018-02-11 23:18:10 +1100

    Finished Day 24

---
+++ 30Days/Day 24 - More Linked Lists/linked.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self, head):
+        curr = head
+
+        while curr and curr.next:
+            while curr.next and curr.data == curr.next.data:
+                curr.next = curr.next.next
+
+            curr = curr.next
+
+        return head
+
+
+
+def main():
+    mylist= Solution()
+    T=int(input())
+    head=None
+    for i in range(T):
+        data=int(input())
+        head=mylist.insert(head,data)    
+    head=mylist.removeDuplicates(head)
+    mylist.display(head); 
+
+if __name__ == '__main__':
+    main()
