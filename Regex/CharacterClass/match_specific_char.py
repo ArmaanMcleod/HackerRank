5bf2fbc6ac337b8caf250ded4a604c62de9462e1
OpticGenius <opticgenius@hotmail.com>
2018-08-04 02:07:09 +1000

    Fixed specific character matching solution

--- Regex/CharacterClass/match_specific_char.py
+++ Regex/CharacterClass/match_specific_char.py
@@ -1,5 +1,5 @@
 import re
 
-Regex_Pattern = r'__________'	# Do not delete 'r'.
+Regex_Pattern = r'^[1-3][0-2][xs0][30Aa][xsu][\.,]$'	# Do not delete 'r'.
 
 print(str(bool(re.search(Regex_Pattern, input()))).lower())
